96523aa841e98ec51e8826fb35908177608e8775
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-08-12 22:32:24 +0530

    Added two problems TODO : Solve sub-array problem

---
+++ javascript/DataStructures/Arrays/SubArraysWithGivenSum.js
@@ -0,0 +1,10 @@
+/*
+    Given an unsorted array of non-negative integers, find all the subarrays whose sum is a given number K
+    Example :
+    Input -
+        K = 12
+        array = [1,2,3,7,5]
+*/
+
+
+
