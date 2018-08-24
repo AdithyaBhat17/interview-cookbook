2e975d251537b40f9f2748b61cd016dc979af8d2
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2018-08-24 14:14:30 +0530

    Added key points to documentation of Selection Sort

--- java/sort/SelectionSort.java
+++ java/sort/SelectionSort.java
@@ -5,11 +5,15 @@ import java.util.Arrays;
 public class SelectionSort {
       /*
        * 1. Selection sort is the simplest sorting algorithm. It finds the smallest element in the array and swaps it with the first position in the array. Then the second smallest and swaps it with the second position etc.
-       * 2. Worst Case Time Complexity : O(n2)
-       * 3. Best Case Time Complexity : O(n2)
-       * 4. Average Time Complexity : O(n2)
+       * 2. Worst Case Time Complexity : O(n^2)
+       * 3. Best Case Time Complexity : O(n^2)
+       * 4. Average Time Complexity : O(n^2)
        * 5. Space Complexity : O(1)
        * This makes it inefficient to operate on larger datasets
+       * 
+       * Key Points
+       * -> Default implementation is not stable.
+       * -> In place sorting algorithm.
        * */
       public static int[] selectionSort(int[] arr) {
           if(arr.length == 1) return arr;
