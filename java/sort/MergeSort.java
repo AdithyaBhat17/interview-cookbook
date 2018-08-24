067594c5df49cbac85290c7ce374c0334ee3471e
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2018-08-24 12:56:08 +0530

    Added key points to documentation of Merge Sort

--- java/sort/MergeSort.java
+++ java/sort/MergeSort.java
@@ -3,13 +3,19 @@ package sort;
 import java.util.Arrays;
 
 public class MergeSort {
-    /*
+   /*
      * Merge sort uses divide & conquer method by splitting the array into smaller parts until there is only one element.
      * The elements are then compared and sorted and merged.
      * Split the arr into half by finding the median.
      * Call mergeSort on the first half and mergeSort on the second half. Then merge the two halves.
      * The time complexity of merge sort is always O(n log n): in all 3 cases (worst, average and best) as merge sort
      * always divides the array in two halves and take linear time to merge two halves.
+     * 
+     * Key Points
+     * -> Merge Sort unlike Quick Sort is not in place sorting algorithm.
+     * -> Merge Sort is stable.
+     * -> Merge Sort is better when data is huge and stored on external storage.
+     * -> Merge Sort requires O(N) extra storage, N being the array size. 
      * */
     public static int[] mergeSort(int[] arr, int lowerIndex, int upperIndex) {
         if(arr.length == 1) return arr;
