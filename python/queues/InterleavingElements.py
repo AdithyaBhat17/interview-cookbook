6f34d6869cd5dfe684e14c5233e493372d5aa955
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-09-05 21:48:46 +0530

    added question

--- python/queues/InterleavingElements.py
+++ python/queues/InterleavingElements.py
@@ -1,3 +1,15 @@
+'''
+Given a queue of integers of even length, rearrange the elements by interleaving the first half of the queue with the second half of the queue. We are allowed to use only queue data structure.
+
+Examples:
+
+Input :  1 2 3 4
+Output : 1 3 2 4
+
+Input : 11 12 13 14 15 16 17 18 19 20
+Output : 11 16 12 17 13 18 14 19 15 20
+'''
+
 # Python3 program for array implementation of queue
 
 # Class Queue to represent a queue
