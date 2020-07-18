c9a60a942635f556a536f2e96d905d3d8f5843c6
adithyabhat17 <adithyabhat17.ab@gmail.com>
2020-07-18 20:09:54 +0530

    🔥 completed subsets of an array problem

--- javascript/DataStructures/Arrays/subsets.js
+++ javascript/DataStructures/Arrays/subsets.js
@@ -1,1 +1,31 @@
-// @todo complete subsets of an array problem
+/*
+
+Given a set of distinct integers, numbers, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: numbers = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+*/
+
+((numbers) => {
+  return nums.reduce(
+    (subsets, current) => {
+      return subsets.concat(subsets.map((subset) => [...subset, current]));
+    },
+    [[]]
+  );
+})([1, 2, 3]);
